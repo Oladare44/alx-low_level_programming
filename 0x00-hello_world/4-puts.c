04cb81e818782dd31ef12d0cf7289b387de4ed3e
Oladare44 <abdullateefoladare@gmail.com>
2022-12-11 00:45:57 -0800

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- * main: entry point 
- * return : successful
+ * main: Entry point 
+ *
+ * Return: return(0) : Successful
  */
-
 int main(void)
 {	
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
