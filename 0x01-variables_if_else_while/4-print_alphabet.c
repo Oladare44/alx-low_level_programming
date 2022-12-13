daecb19778f4837acc4c5783f476133abd9bda61
Oladare44 <abdullateefoladare@gmail.com>
2022-12-12 20:51:34 -0800

    4-alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main : Main Entry
+ *
+ * Return: return 0 - Successful
+ */
+int main(void)
+{
+	char c;
+
+	char = 'a';
+	while 
+		(c <= 'Z') {
+			if ((c != 'q' && c != 'e') && c <= 'z')
+				putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
+
