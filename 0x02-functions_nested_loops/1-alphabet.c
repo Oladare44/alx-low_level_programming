19edebcbf2e4f2c7e6cd93ffa0ca1776187647ca
Oladare44 <abdullateefoladare@gmail.com>
2022-12-13 11:45:27 -0800

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * Print alphabet - Entry Point
+ *
+ * Return - return 0 (successful)
+ */
+
+/* prints alphabets*/
+
+void print_alphabet(void)
+
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+
+	_putchar('\n');
+}
+
