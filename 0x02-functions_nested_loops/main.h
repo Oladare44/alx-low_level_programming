f0084969416866ef5bbde9e5aa337a0d8c23203b
Oladare44 <abdullateefoladare@gmail.com>
2022-12-13 11:08:41 -0800

    putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
-int _putchar(char c);
+int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -13,7 +13,7 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-void print_times_table(int, n);
+void print_times_table(int n);
 void num(int r, int n);
 
 #endif
