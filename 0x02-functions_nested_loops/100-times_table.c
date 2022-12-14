79a1f90e64a58e0b37366bcd094e650fc7a9e912
Oladare44 <abdullateefoladare@gmail.com>
2022-12-13 22:20:31 -0800

    times table for 100

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_times_table - Entry point
+ * @n: input
+ * main - check the code.
+ * Return: Always 0.
+ */
+
+void print_times_table(int n)
+{
+	return (0);
+}
