1c6cec5db9683076c615e1f9cc72e7456616f80f
Oladare44 <abdullateefoladare@gmail.com>
2022-12-13 16:58:27 -0800

    ads

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,6 +2,8 @@
 /**
  * add - a function that adds two integers and returns the result
  *  main - check the code
+ *  @one: first number input
+ *  @two: second number input
  *  Return: Always 0.
  */
 
