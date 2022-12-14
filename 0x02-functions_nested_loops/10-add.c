7b24ef7407b12b65cf7b110679001efed66d41d8
Oladare44 <abdullateefoladare@gmail.com>
2022-12-13 16:54:11 -0800

    ads

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - a function that adds two integers and returns the result
+ *  main - check the code
+ *  Return: Always 0.
+ */
+
+int add(int one, int two)
+{
+	return (one + two);
+}
+
