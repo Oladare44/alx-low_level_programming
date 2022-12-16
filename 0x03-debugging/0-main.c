b0b17faf839b9b39a1b9db174a3d18ef8453b822
Oladare44 <abdullateefoladare@gmail.com>
2022-12-16 09:03:03 -0800

    0-task

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,18 +1,15 @@
 #include "main.h"
 
 /**
- *
- * main -  Test functions for positive and negative
- * 
+ * main - Test functions for positive and negative
  * Return: 0
  *
  */
 int main(void)
 {
-      int i;
-      i = 98;	       
-     
-      positive_or_negative(i);
-    
-      return (0);
+	int i;
+	i = 98;	
+	positive_or_negative(i);
+
+	return (0);
 }
