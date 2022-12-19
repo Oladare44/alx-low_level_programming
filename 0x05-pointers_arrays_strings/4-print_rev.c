10982b4602bcaf583a2bb65a07b246fd747390b7
Oladare44 <abdullateefoladare@gmail.com>
2022-12-19 10:38:08 -0800

    push

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, followed by a new line.
+ * @s: input string to print.
+ * Return: Always 0.
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	while (s[i] != '\0')
+	{
+		_putchar(s[i--]);
+	}
+	_putchar('\n');
+
